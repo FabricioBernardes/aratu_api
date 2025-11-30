fd015d5790e7c5a392d461fd3f1738a9c3e0aa64
Fabrício Bernardes <fbernardes.dev@gmail.com>
2025-11-30 13:15:18 -0300

    chore: Update seeds.rb to include user creation with Devise attributes

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,27 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.find_or_create_by!(email: 'normal_user@example.com') do |user|
+  user.password = 'password123'
+  user.password_confirmation = 'password123'
+end
+
+User.find_or_create_by!(email: 'user_recoverable@example.com') do |user|
+  user.password = 'password123'
+  user.password_confirmation = 'password123'
+  user.reset_password_token = Devise.token_generator.generate(User, :reset_password_token).first
+  user.reset_password_sent_at = Time.now
+end
+
+User.find_or_create_by!(email: 'user_rememberable@example.com') do |user|
+  user.password = 'password123'
+  user.password_confirmation = 'password123'
+  user.remember_created_at = Time.now
+end
+
+User.find_or_create_by!(email: 'user_recent@example.com') do |user|
+  user.password = 'password123'
+  user.password_confirmation = 'password123'
+  user.created_at = Time.now
+end
